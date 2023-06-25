aaee76c73e5414fba18d8c15de037cb95228c806
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2023-06-25 13:58:50 -0300

    Slime toma dano

--- mata-mata/Assets/Scripts/PlayerController.cs
+++ mata-mata/Assets/Scripts/PlayerController.cs
@@ -1,87 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    /*bool isMoving{
-        set{
-            isMoving = value;
-            _playerAnimator.SetBool("isMoving". isMoving);
-        }
-    }*/
     private Rigidbody2D _playerRigidbody2D;
     private Animator _playerAnimator;
-    public float _playerPosition;
+    private SpriteRenderer _spriteRenderer;
+    private Collider2D _swordCollider;
+    private bool _canMove = true;
+
     public float _playerSpeed;
     public float maxSpeed = 8f;
-    public float idleFriction = 0.9f;   
-    //private Vector2 _playerDirection;//determina os eixos 
+    public float idleFriction = 0.9f;
 
-    public GameObject swordHitBox;
-    
-    Vector2 moveInput = Vector2.zero;
-    SpriteRenderer spriteRenderer;
-    bool canMove = true;
+    private Vector2 _moveInput = Vector2.zero;
 
-    Collider2D swordCollider;
-    // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        swordCollider = swordHitBox.GetComponent<Collider2D>();
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _swordCollider = GetComponentInChildren<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update(){
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    void Update()
+    {
+        _moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if(moveInput.sqrMagnitude > 0){
+        if (_moveInput.sqrMagnitude > 0)
+        {
             _playerAnimator.SetInteger("Movimento", 1);
         }
-        else{
+        else
+        {
             _playerAnimator.SetInteger("Movimento", 0);
         }
 
-    }
-
-    void FixedUpdate() {
-        if(canMove == true && moveInput != Vector2.zero) {
-            // Move animation and add velocity
-
-            // Accelerate the player while run direction is pressed
-            // BUT don't allow player to run faster than the max speed in any direction
-           // _playerRigidbody2D.velocity = _playerRigidbody2D.velocity(_playerRigidbody2D.MovePosition + (moveInput * _playerSpeed * Time.deltaTime), maxSpeed);
-            _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + moveInput * _playerSpeed * Time.deltaTime);
-            // Control whether looking left or right
-            if(moveInput.x > 0) {
-                spriteRenderer.flipX = false;
-            } else if (moveInput.x < 0) {
-                spriteRenderer.flipX = true;
-            }
-
-            //isMoving = true;
-
-        } else {
-            // No movement so interpolate velocity towards 0
-            _playerRigidbody2D.velocity = Vector2.Lerp(_playerRigidbody2D.velocity, Vector2.zero, idleFriction);
-            //isMoving = false;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnFire();
         }
-        
     }
 
-    void OnFire(){
+    void FixedUpdate()
+    {
+        if (_canMove && _moveInput != Vector2.zero)
+        {
+            _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _moveInput * _playerSpeed * Time.deltaTime);
+
+            if (_moveInput.x > 0)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            else if (_moveInput.x < 0)
+            {
+                _spriteRenderer.flipX = true;
+            }
+        }
+        else
+        {
+            _playerRigidbody2D.velocity = Vector2.Lerp(_playerRigidbody2D.velocity, Vector2.zero, idleFriction);
+        }
+    }
+
+    void OnFire()
+    {
         _playerAnimator.SetTrigger("swordAttack");
     }
 
-    void LockMovement(){
-        canMove = false;
+    void LockMovement()
+    {
+        _canMove = false;
     }
 
-    void UnlockMovement(){
-        canMove = true;
+    void UnlockMovement()
+    {
+        _canMove = true;
     }
 }
